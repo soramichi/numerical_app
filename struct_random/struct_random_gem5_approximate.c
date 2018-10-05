3f25c271b1534a5ffa53306fb2295df595384b94
Soramichi Akiyama <akiyama@m.soramichi.jp>
2018-10-05 14:48:48 +0900

    add struct_radom

---
+++ struct_random/struct_random_gem5_approximate.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include "mmap_allocator.h"
+
+struct person {
+  int id;
+  //  double score;
+  double* score;
+};
+
+int main(int argc, char* argv[]) {
+  int size, n_access, i;
+  struct person* people;
+  double* values;
+  struct timeval start, end;
+  
+  srand(time(NULL));
+
+  if(argc < 3) {
+    fprintf(stderr, "Usage: %s size n_access\n", argv[0]);
+    return -1;
+  }
+  else {
+    size = atoi(argv[1]);
+    n_access = atoi(argv[2]);
+    
+  }
+
+  printf("size: %d, n_access: %d\n", size, n_access);
+  printf("Allocated critical memory: %d\n", sizeof(struct person) * size);
+  printf("Allocated approximate memory: %d\n", sizeof(double) * size);
+
+  people = (struct person*)mm_malloc_normal(sizeof(struct person) * size);
+  values = (double*)mm_malloc_approximate(sizeof(double) * size);
+
+  printf("people: %llu, values: %llu\n", people, values);
+
+  gettimeofday(&start, NULL);
+  for(i=0; i<size; i++) {
+    people[i].id = i;
+    people[i].score = values + i;
+    *(people[i].score) = (double)i;
+  }
+  gettimeofday(&end, NULL);
+
+  printf("init took %d us\n", ((end.tv_sec * 1000ULL * 1000ULL) + end.tv_usec) - ((start.tv_sec * 1000ULL * 1000ULL) + start.tv_usec));
+
+  gettimeofday(&start, NULL);
+  double ans = 0.0;
+  for(i=0; i<n_access; i++) {
+    int target = rand() % size;
+    ans += *(people[target].score);
+  }
+  gettimeofday(&end, NULL);
+
+  printf("access took %d us\n", ((end.tv_sec * 1000ULL * 1000ULL) + end.tv_usec) - ((start.tv_sec * 1000ULL * 1000ULL) + start.tv_usec));
+  printf("ans: %f\n", ans);
+
+  return 0;
+}
