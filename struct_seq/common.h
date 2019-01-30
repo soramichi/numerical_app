4c5a0b3b8d4886611e4616698b20f82fcc16aef7
Soramichi Akiyama <akiyama@m.soramichi.jp>
2019-01-30 16:17:32 +0900

    improve common.h

--- struct_seq/common.h
+++ struct_seq/common.h
@@ -1,7 +1,13 @@
 #if !defined(__COMMON_H)
 #define __COMMON_H
 
-typedef unsigned long id_type;
-typedef unsigned long score_type;
+#define ID_TYPE unsigned int
+#define SCORE_TYPE unsigned int
+
+typedef ID_TYPE id_type;
+typedef SCORE_TYPE score_type;
+
+#undef ID_TYPE
+#undef SCORE_TYPE
 
 #endif
